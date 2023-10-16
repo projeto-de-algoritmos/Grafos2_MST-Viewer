373dd5b1bb8fdc7b0378c405dfafde53fd06e4d6
PedroLucasCM <pcassianomartins@gmail.com>
2023-10-16 18:45:36 -0300

    clicar agora cria um nó e aresta

--- src/grafo.py
+++ src/grafo.py
@@ -1,32 +1,35 @@
-import networkx as nx
+from networkx import Graph, get_edge_attributes, circular_layout
 
 
 class Grafo:
-    grafo: nx.Graph()
+    grafo: Graph()
     pos: dict
 
     def __init__(self) -> None:
-        self.grafo = nx.Graph()
+        self.grafo = Graph()
         self.pos = {}
 
     def criar_grafo(self, entrada):
         for aresta in entrada:
-            no1, no2, peso = aresta.strip().split(' ')
+            no1, no2, peso = aresta.strip().split(" ")
             try:
                 peso = int(peso)
             except ValueError:
                 return False
             self.criar_aresta(no1, no2, peso)
-    
-        self.pos = nx.circular_layout(self.grafo)
+
+        self.pos = circular_layout(self.grafo)
         return True
-    
+
     def criar_no(self, nome: str, posicao: tuple) -> None:
         self.grafo.add_node(nome, pos=posicao)
 
     def reset(self) -> None:
         self.grafo.clear()
         self.pos.clear()
+
+    def get_nos(self):
+        return list(self.grafo.nodes)
 
     def get_arestas(self):
         return list(self.grafo.edges)
@@ -43,31 +46,33 @@ class Grafo:
     def criar_mst(self):
         mst, peso_total = self.prim()
         return mst, peso_total
-    
+
     def prim(self):
-        mst = nx.Graph()
+        mst = Graph()
         no_inicial = list(self.grafo.nodes())[0]
         nos_mst = set([no_inicial])
         peso_total = 0
 
-        arestas = [(no_inicial, vizinho, data['weight']) for vizinho, data in self.grafo[no_inicial].items()]
+        arestas = [
+            (no_inicial, vizinho, data["weight"])
+            for vizinho, data in self.grafo[no_inicial].items()
+        ]
         arestas.sort(key=lambda x: x[2])
 
         while len(nos_mst) < len(self.grafo.nodes()):
             u, v, weight = arestas.pop(0)
-            
+
             if v not in nos_mst:
                 mst.add_edge(u, v, weight=weight)
                 nos_mst.add(v)
                 peso_total += weight
-                
+
                 for vizinho, data in self.grafo[v].items():
                     if vizinho not in nos_mst:
-                        arestas.append((v, vizinho, data['weight']))
+                        arestas.append((v, vizinho, data["weight"]))
                         arestas.sort(key=lambda x: x[2])
 
         return mst, peso_total
 
-    
     def get_rotulo(self):
-        return nx.get_edge_attributes(self.grafo, 'weight')
+        return get_edge_attributes(self.grafo, "weight")
--- src/interface.py
+++ src/interface.py
@@ -32,36 +32,42 @@ class InterfaceGrafica:
 
     def criar_janelas(self):
         esquerda = ttk.Frame(self.root)
-        esquerda.pack(side="left", padx=self.small_pad, pady=self.small_pad, expand=True)
+        esquerda.pack(
+            side="left", padx=self.small_pad, pady=self.small_pad, expand=True
+        )
 
         direita = ttk.Frame(self.root, height=400, width=650, style="Custom.TFrame")
-        direita.pack(side="right", padx=self.small_pad, pady=self.small_pad, expand=True)
+        direita.pack(
+            side="right", padx=self.small_pad, pady=self.small_pad, expand=True
+        )
 
         return esquerda, direita
 
     def inserir_widget(self, janela: ttk.Frame, args: dict):
         rotulo = ttk.Label(janela, text=args["rotulo"])
         rotulo.pack()
-        
+
         entrada = ttk.Entry(janela, width=args["entrada"])
         entrada.pack(pady=self.small_pad)
 
         if "botao" in args:
-            botao = ttk.Button(janela, text=args["botao"], command=args["comando_botao"])
+            botao = ttk.Button(
+                janela, text=args["botao"], command=args["comando_botao"]
+            )
             botao.pack()
-        
+
         spacer = tk.Label(janela, text="", height=args["espacamento"])
         spacer.pack()
 
-        return entrada 
-        
+        return entrada
+
     def criar_widgets(self):
         janela_esq, janela_dir = self.criar_janelas()
 
         # Widget de inserir arestas ----------------------------------------------------------------------
         inserir_arestas = {
-            "rotulo": "Insira as arestas: (Ex: nó1 nó2 peso, ...)", 
-            "entrada": 40, 
+            "rotulo": "Insira as arestas: (Ex: nó1 nó2 peso, ...)",
+            "entrada": 40,
             "botao": "Inserir",
             "comando_botao": self.criar_grafo,
             "espacamento": 2,
@@ -87,18 +93,23 @@ class InterfaceGrafica:
         self.peso_entrada = self.inserir_widget(janela_esq, escolher_peso)
 
         # Widget da MST --------------------------------------------------------------------------------
-        separator = tk.Frame(janela_esq, bd=10, relief='sunken', height=2, background='black')
-        separator.pack(side='top', fill='x')
+        separator = tk.Frame(
+            janela_esq, bd=10, relief="sunken", height=2, background="black"
+        )
+        separator.pack(side="top", fill="x")
 
         botao_frame = ttk.Frame(janela_esq)
         botao_frame.pack(pady=self.small_pad)
 
-        limpar_botao = ttk.Button(botao_frame, text="Limpar",  command=self.reset, bootstyle="danger")
-        limpar_botao.pack(side= "left", padx=self.small_pad, fill="both", expand=True)
+        limpar_botao = ttk.Button(
+            botao_frame, text="Limpar", command=self.reset, bootstyle="danger"
+        )
+        limpar_botao.pack(side="left", padx=self.small_pad, fill="both", expand=True)
 
-        mst_botao = ttk.Button(botao_frame, text="Gerar MST",  command=self.exibir_mst, bootstyle="success")
-        mst_botao.pack(side= "left", padx=self.small_pad, fill="both", expand=True)
-
+        mst_botao = ttk.Button(
+            botao_frame, text="Gerar MST", command=self.exibir_mst, bootstyle="success"
+        )
+        mst_botao.pack(side="left", padx=self.small_pad, fill="both", expand=True)
 
         # Widget da Figura --------------------------------------------------------------------------------
         self.figura = Figure(figsize=(5, 4), dpi=100)
@@ -106,23 +117,23 @@ class InterfaceGrafica:
         self.grafo_canvas_widget = self.grafo_canvas.get_tk_widget()
         self.grafo_canvas_widget.pack(padx=10, pady=10)
 
+        self.grafo_canvas.mpl_connect("button_press_event", self.on_click)
+
     def reset(self):
         self.grafo.reset()
         self.exibir_grafo()
 
     def criar_grafo(self):
         if self.arestas_entrada.get():
-            if self.grafo.criar_grafo(self.arestas_entrada.get().split(',')):
+            if self.grafo.criar_grafo(self.arestas_entrada.get().split(",")):
                 self.exibir_grafo()
                 self.arestas_entrada.delete(0, "end")
             else:
-                tk.messagebox.showwarning(
-                    "Aviso", "O peso deve ser um número inteiro."
-                )
+                tk.messagebox.showwarning("Aviso", "O peso deve ser um número inteiro.")
 
     def exibir_grafo(self):
         self.figura.clear()
-        
+
         nx.draw(
             G=self.grafo.grafo,
             pos=self.grafo.pos,
@@ -136,7 +147,7 @@ class InterfaceGrafica:
             edge_color=self.edge_color,
             ax=self.figura.add_subplot(111),
         )
-        
+
         # Desenhar labels de arestas TODO: desativar atraves de um switch
         edge_labels = self.grafo.get_rotulo()
         nx.draw_networkx_edge_labels(
@@ -148,9 +159,9 @@ class InterfaceGrafica:
             rotate=False,
             edge_labels=edge_labels,
             font_color=self.edge_label_color,
-            ax=self.figura.gca(), 
+            ax=self.figura.gca(),
         )
-        
+
         self.grafo_canvas.draw()
 
     def exibir_mst(self):
@@ -173,15 +184,13 @@ class InterfaceGrafica:
                 self.grafo_canvas.draw()
                 tk.messagebox.showinfo(
                     "Sucesso", f"Soma dos pesos das arestas utilizadas: {peso_total}"
-                )  
+                )
             else:
                 tk.messagebox.showwarning(
-                "Aviso", "Para gerar uma MST, o grafo deve ser conectado."
-            )  
+                    "Aviso", "Para gerar uma MST, o grafo deve ser conectado."
+                )
         else:
-            tk.messagebox.showwarning(
-                "Aviso", "Crie um grafo antes de exibir a MST."
-            )   
+            tk.messagebox.showwarning("Aviso", "Crie um grafo antes de exibir a MST.")
 
     def encontrar_no_clicado(self, event):
         if self.grafo.pos is not None:
@@ -190,7 +199,7 @@ class InterfaceGrafica:
                 for node, pos in self.grafo.pos.items():
                     node_x, node_y = pos
                     # Vamos considerar que o nó tem um raio de 0.1 para clique
-                    if (x - node_x) ** 2 + (y - node_y) ** 2 <= 0.1 ** 2:
+                    if (x - node_x) ** 2 + (y - node_y) ** 2 <= 0.1**2:
                         return node
         return None
 
@@ -199,8 +208,8 @@ class InterfaceGrafica:
             self.primeiro_no_selecionado = no_selecionado
         else:
             segundo_no_selecionado = no_selecionado
-            self.grafo.add_edge(
-                self.primeiro_no_selecionado, segundo_no_selecionado, weight=peso
+            self.grafo.criar_aresta(
+                self.primeiro_no_selecionado, segundo_no_selecionado, peso
             )
             self.grafo.get_arestas().append(
                 (self.primeiro_no_selecionado, segundo_no_selecionado, peso)
@@ -212,25 +221,31 @@ class InterfaceGrafica:
         x, y = event.xdata, event.ydata
         if x is not None and y is not None:
             no_clicado = self.encontrar_no_clicado(event)
-            
+
             if no_clicado is not None:
                 peso_entrada = self.peso_entrada.get()
                 peso = int(peso_entrada) if peso_entrada.isdigit() else 1
                 self.conectar_nos(no_clicado, peso)
             else:
-                node_label = f"{len(self.grafo.nodes) + 1}"
-                self.grafo.add_node(node_label, pos=(x, y))
+                node_label = f"{len(self.grafo.get_nos()) + 1}"
+                i = 1
+                for no in self.grafo.grafo.nodes:
+                    if node_label == self.grafo.grafo.nodes[no].get("name", None):
+                        i += 1
+                    else:
+                        node_label = f"{len(self.grafo.get_nos()) + i}"
+                self.grafo.criar_no(node_label, posicao=(x, y))
                 self.grafo.pos[node_label] = (x, y)
                 self.exibir_grafo()
-                
+
                 if self.primeiro_no_selecionado is not None:
                     segundo_no_selecionado = node_label
                     peso_entrada = self.peso_entrada.get()
                     peso = int(peso_entrada) if peso_entrada.isdigit() else 1
-                    self.grafo.add_edge(
+                    self.grafo.criar_aresta(
                         self.primeiro_no_selecionado,
                         segundo_no_selecionado,
-                        weight=peso
+                        peso,
                     )
                     self.grafo.get_arestas().append(
                         (self.primeiro_no_selecionado, segundo_no_selecionado)
@@ -239,7 +254,7 @@ class InterfaceGrafica:
                     self.exibir_grafo()
                 else:
                     self.primeiro_no_selecionado = node_label
-   
+
     def remover_no(self):
         no_a_remover = self.remove_entrada.get()
 
@@ -249,13 +264,6 @@ class InterfaceGrafica:
             del self.grafo.pos[no_a_remover]
             self.remove_entrada.delete(0, "end")
         else:
-            tk.messagebox.showwarning(
-                "Aviso", "O nó deve estar presente no grafo."
-            )
+            tk.messagebox.showwarning("Aviso", "O nó deve estar presente no grafo.")
 
         self.exibir_grafo()
-
-# apenas para debug 
-root = tk.Tk()
-app = InterfaceGrafica(root)
-root.mainloop()
